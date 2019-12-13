05b4ecd5eb5a633913b91de5baff11cbfcd414b8
chepkiruibetty <chepkiruibetty64@gmail.com>
2019-12-13 15:19:09 +0300

    calculate the functionality

--- js/script.js
+++ js/script.js
@@ -1,37 +1,47 @@
-// declaring variables 
-var pizzaFlavour=[
-"Berry",
-"Butternut Squash",
-"Chicken",
-"Hummus",
-"Turkish Ground",
-];
-var pizzaSize=[
-  "Regular",
-  "Medium" , 
-  "Large",
-];
-var pizzaCrust=[
- "Thin crust",
- "Thick crust",
- "Deep crust",
- "Stuffed crust"
-]; 
-var pizzaTopping=[
-  "onion",
-  "Mushroom",
-  "bacon",
-  "black olives",
-];
+// // declaring variables
+// var pizzaFlavour = [
+//   "Berry",
+//   "Butternut Squash",
+//   "Chicken",
+//   "Hummus",
+//   "Turkish Ground"
+// ];
+// var pizzaSize = ["Regular", "Medium", "Large"];
+// var pizzaCrust = ["Thin crust", "Thick crust", "Deep crust", "Stuffed crust"];
+// var pizzaTopping = ["onion", "Mushroom", "bacon", "black olives"];
 
 // calling functions
-
-var flavour=document.getElementById("pizzaflavour").value;
-var size=document.getElementById("pizzasize").value;
-var crust=document.getElementById("pizzacrust").value;
-var topping=document.getElementById("pizzatopping").value;
-var number=document.getElementById("")
-
-function getAmount(){
-
+function getFlavour() {
+  var flavour = document.getElementById("pizzaflavour").value;
+  return parseInt(flavour);
+}
+function getSize() {
+  var size = document.getElementById("pizzasize").value;
+  return parseInt(size);
+}
+function getCrust() {
+  var crust = document.getElementById("pizzacrust").value;
+  return parseInt(crust);
+}
+function getTopping() {
+  var topping = document.getElementById("pizzatopping").value;
+  return parseInt(topping);
+}
+function getNumber() {
+  var number = document.getElementById("number").value;
+  return parseInt(number);
+}
+function getAmount() {
+  var results =
+    (getFlavour() + getSize() + getCrust() + getTopping()) * getNumber();
+  alert(
+    "You've ordered " +
+      getNumber("") +
+      " pizza," +
+      "which amounts to Ksh. " +
+      results +
+      " Thanks"
+  );
+  prompt("Enter your location");
+  alert("order delivered now.delivery fee is 50/=");
 }
