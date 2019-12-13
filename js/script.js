01ed8a07fc840fd47902b3f98d12beff16c82bf5
chepkiruibetty <chepkiruibetty64@gmail.com>
2019-12-13 11:09:09 +0300

    declared the arrays

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// declaring variables 
+var pizzaFlavour=[
+"Berry",
+"Butternut Squash",
+"Chicken",
+"Hummus",
+"Turkish Ground",
+];
+var pizzaSize=[
+  "Regular",
+  "Medium" , 
+  "Large",
+];
+var pizzaCrust=[
+ "Thin crust",
+ "Thick crust",
+ "Deep crust",
+ "Stuffed crust"
+]; 
+var pizzaToppings=[
+  "onion",
+  "Mushroom",
+  "bacon",
+  "black olives",
+];
+
+// calling functions
+
+var flavour=document.getElementById("pizzaflavour").value;
+var size=document.getElementById("pizzasize").value;
+var 
